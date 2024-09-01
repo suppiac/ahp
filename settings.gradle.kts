9a847eeb11077316689854af7b0b209808091403
suppiac <18070283453@163.com>
2024-09-02 00:08:26 +0800

    update version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,13 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 pluginManagement {
     repositories {
         gradlePluginPortal()
+        mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 
     plugins {
         kotlin("jvm").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.kotlin.plugin.compose").version(extra["kotlin.version"] as String)
         id("org.jetbrains.compose").version(extra["compose.version"] as String)
     }
 }
